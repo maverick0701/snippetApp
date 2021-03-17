6ed8c79ade79a6d88f8ac7fb4feed8ee3ee23ca5
@maverick0701 <madhavanandcp070@gmail.com>
2021-03-17 22:54:06 +0530

    implimented oAuth 1

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, createContext } from "react";
 
 class Login extends Component {
   constructor(props) {
---
+++ src/reducers/auth.js
@@ -0,0 +1,36 @@
+import {
+  LOGIN_FAILED,
+  LOGIN_START,
+  LOGIN_SUCCESS,
+} from "../actions/actionTypes";
+const initialAuthState = {
+  user: {},
+  isLoggedIn: false,
+  inProgress: false,
+  error: null,
+};
+export default function auth(state = initialAuthState, action) {
+  switch (action.types) {
+    case LOGIN_START:
+      return {
+        ...state,
+        inProgress: true,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        user: action.user,
+        isLoggedIn: true,
+        inProgress: false,
+        error: null,
+      };
+    case LOGIN_FAILED:
+      return {
+        ...state,
+        inProgress: false,
+        error: action.error,
+      };
+    default:
+      return state;
+  }
+}
